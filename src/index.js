0de10af19c3b44f70015fe79faabe950d0f2b05a
Andrew Cao <andrewcao618@gmail.com>
2024-12-31 16:33:41 -0500

    Separated Content into One file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-import { toggleSidebar, showAddTodoDialog, closeTodoDialog } from "./userInterface";
+import { initializeUI } from "./userInterface";
 
-const toggleSidebarBtns = document.querySelectorAll(".toggle-sidebar");
-const addTodoBtn = document.querySelector(".add-todo");
-const cancelTodoBtn = document.querySelector(".cancel-todo");
-const addTodoDialog = document.querySelector(".add-todo-dialog");
-
-toggleSidebarBtns.forEach((btn) => {
-  btn.addEventListener("click", toggleSidebar);
-});
-
-addTodoBtn.addEventListener("click", showAddTodoDialog);
-cancelTodoBtn.addEventListener("click", closeTodoDialog);
-
-document.addEventListener("click", (event) => {
-  if (event.target === addTodoDialog) {
-    closeTodoDialog();
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  initializeUI();
 });
--- src/userInterface.js
+++ src/userInterface.js
@@ -2,16 +2,57 @@ const sidebar = document.querySelector(".sidebar");
 const addTodoDialog = document.querySelector(".add-todo-dialog");
 
 
-export function toggleSidebar() {
+function toggleSidebar() {
   const mainContainer = document.querySelector(".main-container");
   sidebar.classList.toggle("hidden");
   mainContainer.classList.toggle("full-width");
 }
 
-export function showAddTodoDialog() {
+function showAddTodoDialog() {
   addTodoDialog.showModal();
 }
 
-export function closeTodoDialog() {
+function closeTodoDialog() {
   addTodoDialog.close();
+}
+
+function showProjectDialog() {
+  const projectDialog = document.querySelector(".add-project-dialog");
+  projectDialog.showModal();
+}
+
+function closeProjectDialog() {
+  const projectDialog = document.querySelector(".add-project-dialog");
+  projectDialog.close();
+}
+
+export function initializeUI() {
+  const toggleSidebarBtns = document.querySelectorAll(".toggle-sidebar");
+  const addTodoBtn = document.querySelector(".add-todo");
+  const cancelTodoBtn = document.querySelector(".cancel-todo");
+  const addTodoDialog = document.querySelector(".add-todo-dialog");
+  const addProjectBtn = document.querySelector(".add-project");
+  const cancelProjectBtn = document.querySelector(".cancel-project");
+
+  toggleSidebarBtns.forEach((btn) => {
+    btn.addEventListener("click", toggleSidebar);
+  });
+
+  addTodoBtn.addEventListener("click", showAddTodoDialog);
+  cancelTodoBtn.addEventListener("click", closeTodoDialog);
+
+  document.addEventListener("click", (event) => {
+    if (event.target === addTodoDialog) {
+      closeTodoDialog();
+    }
+  });
+
+  addProjectBtn.addEventListener("click", showProjectDialog);
+  cancelProjectBtn.addEventListener("click", closeProjectDialog);
+
+  document.addEventListener("click", (event) => {
+    if (event.target === addProjectDialog) {
+      closeProjectDialog();
+    }
+  });
 }
