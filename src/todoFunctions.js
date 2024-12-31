40ffb6b60fb6b43da20aa7437de372f0e4340477
Andrew Cao <andrewcao618@gmail.com>
2024-12-31 16:42:53 -0500

    Todo Function Setup

---
+++ src/todoFunctions.js
@@ -0,0 +1,15 @@
+const inbox = [];
+const today = [];
+const tomorrow = [];
+const week = [];
+
+class Project {
+  constructor(title, description, date, priority, locationInbox) {
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+    this.locationInbox = locationInbox;
+  }
+}
+
