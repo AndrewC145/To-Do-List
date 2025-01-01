3dadb6e2ef41931487163c988c094c9bd8bba42b
Andrew Cao <andrewcao618@gmail.com>
2025-01-01 17:58:31 -0500

    Added Edit Todo Popup Function

--- src/userInterface.js
+++ src/userInterface.js
@@ -3,13 +3,14 @@ import { clearInputs } from "./todoFunctions";
 const sidebar = document.querySelector(".sidebar");
 const addTodoDialog = document.querySelector(".add-todo-dialog");
 const projectDialog = document.querySelector(".add-project-dialog");
+const editDialog = document.querySelector(".edit-todo-dialog");
 
 
 function toggleSidebar() {
   const mainContainer = document.querySelector(".main-container");
   sidebar.classList.toggle("hidden");
   mainContainer.classList.toggle("full-width");
-}
+} 
 
 function showAddTodoDialog() {
   addTodoDialog.showModal();
@@ -18,6 +19,14 @@ function showAddTodoDialog() {
 function closeTodoDialog() {
   addTodoDialog.close();
   clearInputs();
+}
+
+function showEditDialog() {
+  editDialog.showModal();
+}
+
+function closeEditDialog() {
+  editDialog.closeModal();
 }
 
 function showProjectDialog() {
@@ -33,6 +42,8 @@ export function initializeUI() {
   const addTodoBtn = document.querySelector(".add-todo");
   const cancelTodoBtn = document.querySelector(".cancel-todo");
   const addTodoDialog = document.querySelector(".add-todo-dialog");
+  const editTodoBtn = document.querySelector(".edit-todo");
+  const cancelEditBtn = document.querySelector(".cancel-edit");
   const addProjectBtn = document.querySelector(".add-project");
   const cancelProjectBtn = document.querySelector(".cancel-project");
 
@@ -43,12 +54,21 @@ export function initializeUI() {
   addTodoBtn.addEventListener("click", showAddTodoDialog);
   cancelTodoBtn.addEventListener("click", closeTodoDialog);
 
+  editTodoBtn.forEach((editBtn) => {
+    editBtn.addEventListener("click", showEditDialog);
+  })
+
+  cancelEditBtn.addEventListener("click", closeEditDialog);
+
   document.addEventListener("click", (event) => {
     if (event.target === addTodoDialog) {
       closeTodoDialog();
     }
     if (event.target === projectDialog) {
       closeProjectDialog();
+    }
+    if (event.target === editDialog) {
+      closeEditDialog();
     }
   });
 
